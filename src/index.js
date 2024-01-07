207826b988467ee0c39ca32bf642586cc1f650c4
Shauna845 <shauna10@hotmail.co.uk>
2024-01-07 15:36:27 +0000

    Added the API and Axios

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "On Turning Her Up in Her Nest with the Plough",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "97a2aef61tb590co334c6f1f08042a43";
+  let prompt = `User Instructions: Please generate a short poem about ${instructionsInput.value}`;
+  let context =
+    "You are an intelligent AI who is an international poet. Your mission is to generate a 4 line poem in basic HTML. Make sure to follow the user instructions below. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside the <i> element";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator");
