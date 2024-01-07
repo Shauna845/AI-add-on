f7c7aa539fc8acc100745349509a9ae4edfe2568
Shauna845 <shauna10@hotmail.co.uk>
2024-01-07 15:59:45 +0000

    Updated html and js

--- src/index.js
+++ src/index.js
@@ -17,6 +17,9 @@ function generatePoem(event) {
     "You are an intelligent AI who is an international poet. Your mission is to generate a 4 line poem in basic HTML. Make sure to follow the user instructions below. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside the <i> element";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.innerHTML = `<div class="blink">⌛ Generating a poem about ${instructionsInput.value}... </div>`;
+
   axios.get(apiUrl).then(displayPoem);
 }
 
